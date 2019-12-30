dced236693cabe25168ec0d7e64112be21747594
wpathan <wpathan@citi-us.com>
2019-12-30 18:45:44 +0530

    upload project into git repo test

---
+++ src/main/java/com/manageid/Helloworld/HelloWorldApplication.java
@@ -0,0 +1,17 @@
+package com.manageid.Helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//ss@ComponentScan (basePackages = {"com.manageid.*"})
+
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
